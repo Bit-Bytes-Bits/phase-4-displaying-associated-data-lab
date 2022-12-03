88343ac4aa50ab1313f1b6b74ee605c22b2289c5
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-12-03 09:18:08 +0300

    Done with assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+def index
+    item = Item.all
+    render json:item, include: :user
+end
 end
